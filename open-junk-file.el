4975991e6bf564ab2f601c7f43a78e05113c9a12
USAMI Kenta <tadsan@zonu.me>
2016-12-10 13:27:04 +0900

    Fix docs by check-doc

--- open-junk-file.el
+++ open-junk-file.el
@@ -26,14 +26,14 @@
 ;;; Commentary:
 ;;
 ;; M-x `open-junk-file' opens a new file whose filename is derived from
-;; current time. You can write short program in it. It helps to
+;; current time.  You can write short program in it.  It helps to
 ;; try-and-error programs.
 ;;
 ;; For example, in Emacs Lisp programming, use M-x `open-junk-file'
-;; instead of *scratch* buffer. The junk code is SEARCHABLE.
+;; instead of *scratch* buffer.  The junk code is SEARCHABLE.
 ;;
 ;; In Ruby programming, use M-x `open-junk-file' and
-;; write a script with xmpfilter annotations. It is the FASTEST
+;; write a script with xmpfilter annotations.  It is the FASTEST
 ;; methodology to try Ruby methods, Irb is not needed anymore.
 ;; Xmpfilter is available at http://eigenclass.org/hiki/rcodetools
 
@@ -130,10 +130,10 @@ Whether the file is a JUNK or not is infered by `open-junk-file-format'.")
 ;;;###autoload
 (defun open-junk-file (&optional format find-file-fn)
   "Open a new file whose filename is derived from current time.
- You can write short program in it. It helps to try-and-error programs.
+You can write short program in it.  It helps to try-and-error programs.
 
 For example, in Emacs Lisp programming, use M-x `open-junk-file'
-instead of *scratch* buffer. The junk code is SEARCHABLE.
+instead of *scratch* buffer.  The junk code is SEARCHABLE.
 
 FORMAT and FIND-FILE-FN are optional.
 Default value of them are `open-junk-file-format' and
